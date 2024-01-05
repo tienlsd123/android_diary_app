78e6c971f9126107a5cf21b8253c989b066e5a48
tienbxdev <141222459+tienbxdev@users.noreply.github.com>
2024-01-05 11:28:53 +0700

    create model diary, enum mood

---
+++ app/src/main/java/com/tienbx/diary/model/Diary.kt
@@ -0,0 +1,19 @@
+package com.tienbx.diary.model
+
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.BsonObjectId
+
+class Diary : RealmObject {
+    @PrimaryKey
+    var _id: BsonObjectId = BsonObjectId()
+    var ownerId: String = ""
+    var mood: String = Mood.Neutral.name
+    var title: String = ""
+    var description: String = ""
+    var images: RealmList<String> = realmListOf()
+    var date: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
+}
